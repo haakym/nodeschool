e2e71a23753d965c5e3ee2559c429dca40c9f0b1
Dominic Jones <haakym@gmail.com>
2016-12-10 19:00:49 +0000

    filtered solution now working, yay for path!

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
-const path = process.argv[2]
+const path = require('path')
+
+const filePath = process.argv[2]
 const ext =  '.' + process.argv[3]
 
-fs.readdir(path, (err, files) => {
+fs.readdir(filePath, (err, files) => {
 	filteredList = files
-		.filter((item) => item.indexOf(ext) !== -1 )
-		.forEach((item) => console.log(item))
-	console.log(filteredList)
+		.filter(item => path.extname(item) === ext)
+		.forEach(item => console.log(item))
 })
